b5296e2370772f72b2a3cbd57d4b8169e1e79781
de_gentlem4n <th3g3n3lm4n@gmail.com>
2020-10-27 22:41:33 -0400

    using blue and focus event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+
+/* 
+    #01
+    Using the focus event lisenter on the nav links
+*/
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('focus', (e) =>{
+    console.log(e.target)
+
+    e.target.style.background = 'pink'
+
+
+},true)
+
+/* 
+    #02
+    Using the blur event to change background color to default
+*/
+
+nav.addEventListener('blur', (e) => {
+    e.target.style.background = ''
+},true)
